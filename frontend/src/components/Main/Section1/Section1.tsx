c1290cfb3dea160c89ca0786403d27928b65fa53
MSaliamon <maksimsalmon181@gmail.com>
2025-10-23 12:58:09 +0300

    section1 done

---
+++ frontend/src/components/Main/Section1/Section1.tsx
@@ -0,0 +1,13 @@
+import './Section1.scss'
+
+function Section1() {
+  return (
+    <section className="section1">
+        <p className="section1__sub-title">Create, explore, & collect digital art NFTs.</p>
+        <h1 className="section1__title">The new creative economy.</h1>
+        <button className="section1__btn button2">Start your search</button>
+    </section>
+  )
+}
+
+export default Section1
